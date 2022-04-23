c5ebcff62e22b73594d0b82c3c434f609e15fa8c
ondra <ondra.szekely@gmail.com>
2022-04-23 23:13:03 +0200

    feat: Add new ops of FS handlers/DB

--- tools/io_handlers/fs_handler.py
+++ tools/io_handlers/fs_handler.py
@@ -15,7 +15,7 @@ class FileSystemIOHandler:
     """
 
     @staticmethod
-    def folder_force_create(folder_path: str) -> None:
+    def force_create_folder(folder_path: str) -> None:
         """
         Creates a folder given by `folder_path`. If there exists a folder on given path, it is deleated and created
         again. If there is a file on given path, an exception (OSError) is rised.
@@ -33,7 +33,7 @@ class FileSystemIOHandler:
         os.mkdir(folder_path)
 
     @staticmethod
-    def folder_create(folder_path: str) -> None:
+    def create_folder(folder_path: str) -> None:
         """
         Creates a folder given by `folder_path`. If there exists a folder on given path, then nothing happens.
         If there is a file on given path, an exception (OSError) is rised.
@@ -49,3 +49,52 @@ class FileSystemIOHandler:
             param_val.folder_existence_check(folder_path)
         else:
             os.mkdir(folder_path)
+
+    @staticmethod
+    def delete_file(file_path: str) -> None:
+        """
+        Deletes a file given by `file_path`. Raises an exception (OSError) if doesn't exist.
+
+        Args:
+            file_path (str): File path.
+
+        Returns (None):
+        """
+        param_val.type_check(file_path, str)
+        param_val.file_existence_check(file_path)
+
+        os.remove(file_path)
+
+    @staticmethod
+    def get_file(file_path: str) -> bytes:
+        """
+        Reads file's bytes. File is given by its path `file_path`.
+
+        Args:
+            file_path (str): File path.
+
+        Returns (bytes): File's bytes.
+        """
+        param_val.type_check(file_path, str)
+        param_val.file_existence_check(file_path)
+
+        with open(file_path, 'rb') as file_reader:
+            file_bytes = file_reader.read()
+            file_reader.close()
+
+        return file_bytes
+
+    @staticmethod
+    def delete_folder(folder_path: str) -> None:
+        """
+        Deletes a folder given by `folder_path`. Raises an exception (OSError) if doesn't exist.
+
+        Args:
+            folder_path (str): Folder path.
+
+        Returns (None):
+        """
+        param_val.type_check(folder_path, str)
+        param_val.folder_existence_check(folder_path)
+
+        rmtree(folder_path)
