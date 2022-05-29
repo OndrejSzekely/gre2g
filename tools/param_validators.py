69e7ee120be3eb71d217f6c91f6941235caed89c
ondra <ondra.szekely@gmail.com>
2022-05-29 18:00:05 +0200

    refa: Imperative for `param_validators` module

--- tools/param_validators.py
+++ tools/param_validators.py
@@ -8,7 +8,7 @@ from typing import Any, Union
 import typeguard
 
 
-def type_check(variable: Any, expected_type: Any) -> None:
+def check_type(variable: Any, expected_type: Any) -> None:
     """
     Validates if given <variable> is type of <expected_type>.
 
@@ -25,7 +25,7 @@ def type_check(variable: Any, expected_type: Any) -> None:
     typeguard.check_type("variable", variable, expected_type)
 
 
-def file_existence_check(file_path: str) -> None:
+def check_file_existence(file_path: str) -> None:
     """
     Validates if file path exists and is a file.
 
@@ -37,14 +37,14 @@ def file_existence_check(file_path: str) -> None:
     Exceptions:
         OSError: If file does not exist or path is not a file.
     """
-    type_check(file_path, str)
+    check_type(file_path, str)
     if not os.path.exists(file_path):
         raise OSError(f"Path `{file_path}` does not exist.")
     if not os.path.isfile(file_path):
         raise OSError(f"Path `{file_path}` exists but it is not a file.")
 
 
-def folder_existence_check(folder_path: str) -> None:
+def check_folder_existence(folder_path: str) -> None:
     """
     Validates if file path exists and is a folder.
 
@@ -56,14 +56,14 @@ def folder_existence_check(folder_path: str) -> None:
     Exceptions:
         OSError: If file does not exist or path is not a folder.
     """
-    type_check(folder_path, str)
+    check_type(folder_path, str)
     if not os.path.exists(folder_path):
         raise OSError(f"Path `{folder_path}` does not exist.")
     if os.path.isfile(folder_path):
         raise OSError(f"Path `{folder_path}` exists but it is not a folder.")
 
 
-def parameter_value_in_range(
+def check_parameter_value_in_range(
     param_value: Union[int, float], lower_bound: Union[int, float], upper_bound: Union[int, float]
 ) -> None:
     """
@@ -79,8 +79,8 @@ def parameter_value_in_range(
     Exceptions:
         ValueError: If value is not in the range.
     """
-    type_check(param_value, (int, float))
-    type_check(lower_bound, (int, float))
-    type_check(upper_bound, (int, float))
+    check_type(param_value, (int, float))
+    check_type(lower_bound, (int, float))
+    check_type(upper_bound, (int, float))
     if param_value < lower_bound or param_value > upper_bound:
         raise ValueError(f"Given `{param_value}` is out of the allowed range" f" <{lower_bound}, {upper_bound}>.")
